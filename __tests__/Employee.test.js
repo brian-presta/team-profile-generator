e72a37b7d86c1b6a9fb93331f9ca462ccd975e2b
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-23 18:49:42 -0700

    engineer test and constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,19 +1,19 @@
 const Employee = require('../lib/Employee')
 
 test('creates an employee object', () => {
-    const employee = new Employee('Robert','Manager','4','robert@email.com')
+    const employee = new Employee('Robert','4','robert@email.com')
 
     expect(employee.name).toBe('Robert')
-    expect(employee.title).toBe('Manager')
+    expect(employee.role).toBe('Employee')
     expect(employee.employeeID).toBe('4')
     expect(employee.email).toBe('robert@email.com')
 })
 
 test('creates HTML content  with the startCard method', () => {
-    const employee = new Employee('Robert','Manager','4','robert@email.com')
+    const employee = new Employee('Robert','4','robert@email.com')
 
     expect(employee.startCard('')).toContain(employee.name)
-    expect(employee.startCard('')).toContain(employee.title)
+    expect(employee.startCard('')).toContain(employee.role)
     expect(employee.startCard('')).toContain(employee.employeeID)
     expect(employee.startCard('')).toContain(employee.email)
     expect(employee.startCard('')).toContain('<div class')
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Engineer = require('../lib/Engineer')
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer('Robert','4','robert@email.com','robert-dev')
+
+    expect(engineer.name).toBe('Robert')
+    expect(engineer.role).toBe('Engineer')
+    expect(engineer.employeeID).toBe('4')
+    expect(engineer.email).toBe('robert@email.com')
+    expect(engineer.gitHub).toBe('robert-dev')
+})
+
+test('creates HTML content  with the makeCard method', () => {
+    const engineer = new Engineer('Robert','4','robert@email.com','robert-dev')
+
+    expect(engineer.makeCard()).toContain(engineer.name)
+    expect(engineer.makeCard()).toContain(engineer.role)
+    expect(engineer.makeCard()).toContain(engineer.employeeID)
+    expect(engineer.makeCard()).toContain(engineer.email)
+    expect(engineer.makeCard()).toContain(engineer.gitHub)
+    expect(engineer.makeCard()).toContain('<div class')
+    expect(engineer.makeCard()).toContain('<ul class')
+    expect(engineer.makeCard()).toContain('<li class')
+    console.log(engineer.makeCard())
+})
