027421bc7f1c9619c21770028b176041d54dd4ac
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-24 11:25:08 -0700

    set up manager test and constructor

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,25 @@
+const Manager = require('../lib/Manager')
+
+test('creates an engineer object', () => {
+    const manager = new Manager('Robert','2','robert@email.com','2')
+
+    expect(manager.name).toBe('Robert')
+    expect(manager.role).toBe('Manager')
+    expect(manager.employeeID).toBe('2')
+    expect(manager.email).toBe('robert@email.com')
+    expect(manager.officeNumber).toBe('2')
+})
+
+test('creates HTML content  with the makeCard method', () => {
+    const manager = new Manager('Robert','2','robert@email.com','2')
+
+    expect(manager.makeCard()).toContain(manager.name)
+    expect(manager.makeCard()).toContain(manager.role)
+    expect(manager.makeCard()).toContain(manager.employeeID)
+    expect(manager.makeCard()).toContain(manager.email)
+    expect(manager.makeCard()).toContain(manager.officeNumber)
+    expect(manager.makeCard()).toContain('<div class')
+    expect(manager.makeCard()).toContain('<ul class')
+    expect(manager.makeCard()).toContain('<li class')
+    console.log(manager.makeCard())
+})
