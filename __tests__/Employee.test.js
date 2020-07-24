5b9399e536b1c39450a2cad618b81d339d6f88e2
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-23 18:18:55 -0700

    finished employee constructor, test for startcard method, startcard method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,15 @@ test('creates an employee object', () => {
     expect(employee.employeeID).toBe('4')
     expect(employee.email).toBe('robert@email.com')
 })
+
+test('creates HTML content  with the startCard method', () => {
+    const employee = new Employee('Robert','Manager','4','robert@email.com')
+
+    expect(employee.startCard('')).toContain(employee.name)
+    expect(employee.startCard('')).toContain(employee.title)
+    expect(employee.startCard('')).toContain(employee.employeeID)
+    expect(employee.startCard('')).toContain(employee.email)
+    expect(employee.startCard('')).toContain('<div class')
+    expect(employee.startCard('')).toContain('<ul class')
+    expect(employee.startCard('')).toContain('<li class')
+})
